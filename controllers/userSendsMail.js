3069db77b213ca140647bb30b8672a6a9977b82e
Dimitris Dafnis <68849116+jim-daf@users.noreply.github.com>
2021-03-20 14:47:31 +0200

    Update userSendsMail.js

--- controllers/userSendsMail.js
+++ controllers/userSendsMail.js
@@ -1,10 +1,11 @@
 const nodemailer = require('nodemailer');
 const mailGun = require('nodemailer-mailgun-transport');
 
+// Authenticate sender's email
 const auth = {
     auth: {
-        api_key: process.env.API_KEY ||  'key-76646de3d9cc50b097e141b833c5c8a7',
-        domain: process.env.DOMAIN || 'sandbox08535a1f8f3643ab8ea3a8429eab2f2e.mailgun.org' 
+        api_key: process.env.API_KEY ||  'insert your mailgun api key',
+        domain: process.env.DOMAIN || 'insert your mailgun domain' 
     }
 };
 
@@ -14,18 +15,18 @@ const transporter = nodemailer.createTransport(mailGun(auth));
 const sendMail = (email, subject, text, cb) => {
     const mailOptions = {
         from: email, 
-        to: 'ece19032@go.uop.gr',  
+        to: "receiver's email",  
         subject,
         text
         
     };
     const mailOptions2 = {
         from: email, 
-        to: 'jimitrakis11@gmail.com',
+        to: "receiver's email",
         subject,
         text
     }
-
+    //Send email
     transporter.sendMail(mailOptions, function (err, data) {
         if (err) {
             return cb(err, null);
@@ -37,7 +38,7 @@ const sendMail = (email, subject, text, cb) => {
             return err
         }
         else{
-            console.log("email sent TO ece19032@go.uop.gr!")
+            console.log("email sent TO receiver's email!")
         }
     });
 }
